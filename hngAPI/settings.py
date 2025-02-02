5401913403358ce0a5c67719457a4eeb9fd3a927
vicoduor <victoroduorr@gmail.com>
2025-02-03 01:09:31 +0300

    .

--- hngAPI/settings.py
+++ hngAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--l76$+t0y@t5(l^9+(fdjc=-l)@_bw(@g&ndb%kh-89$7qbk7w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
